aac2d61dad2bccd142f44fee4be310c266031589
Mohammed Musharraf <musharraf.personal@gmail.com>
2023-12-10 22:57:05 +0530

    created pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import LoginIn from './pages/loginin';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/about'; 
 
 export default function App() {
   return (
-    <div className='text-red-500'>
-      Hello World
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<LoginIn />} />
+        <Route path="/sign-out" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} /> 
+      </Routes>
+    </BrowserRouter>
+  );
 }
