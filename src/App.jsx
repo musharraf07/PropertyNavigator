a2215db28c00f19b8d293440d48ce1bd1f84cc80
Mohammed Musharraf <musharraf.personal@gmail.com>
2023-12-10 23:38:32 +0530

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import LoginIn from './pages/loginin';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/about'; 
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<LoginIn />} />
