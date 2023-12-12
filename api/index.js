a0016c6975e591b48d3718c64335f80cf13af5fd
Mohammed Musharraf <musharraf.personal@gmail.com>
2023-12-12 20:26:37 +0530

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running  on port 3000");
+mongoose.connect(process.env.MONGO, {});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+
+  // Start your server or perform other actions after successful connection
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 });
