358f8708423e6817f41c2df6cd4513401ca743af
Mohammed Musharraf <musharraf.personal@gmail.com>
2023-12-13 17:01:40 +0530

    sign up ui created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import LoginIn from './pages/loginin';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import About from './pages/about'; 
-import Header from './components/Header';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginIn from "./pages/loginin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/about";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<LoginIn />} />
-        <Route path="/sign-out" element={<SignUp />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} /> 
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,14 +7,14 @@ export const signup = async (req, res, next) => {
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newuser = new User({ username, email, password: hashedPassword });
   try {
-    // const existingUsername = await User.findOne({ username });
+    const existingUsername = await User.findOne({ username });
 
-    // const existingUseremail = await User.findOne({ email });
+    const existingUseremail = await User.findOne({ email });
 
-    // if (existingUsername || existingUseremail) {
-    //   // If the username is already taken, send a 409 Conflict response
-    //   return res.status(409).json("Username or email already exists.");
-    // }
+    if (existingUsername || existingUseremail) {
+      // If the username is already taken, send a 409 Conflict response
+      return res.status(409).json("Username or email already exists.");
+    }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
