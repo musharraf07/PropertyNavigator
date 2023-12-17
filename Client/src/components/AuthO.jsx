f2016b1039f47c76e1d80fc6189b47373e181be3
Mohammed Musharraf <musharraf.personal@gmail.com>
2023-12-17 22:07:46 +0530

    competed google oauth feature

--- Client/src/components/AuthO.jsx
+++ Client/src/components/AuthO.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { FaHandPointLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+
 export default function AuthO() {
+  const dispatch = useDispatch();
+  const navigatge = useNavigate();
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
 
       const result = await signInWithPopup(auth, provider);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigatge("/");
     } catch (error) {
       console.log("could not sign in with google", error);
     }
